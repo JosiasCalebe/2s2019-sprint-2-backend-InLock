92dd16c3f63758d9766d2c15329e1fc5bdf1658f
Diego Alvarez Colmenero <46938238804@info.local>
2019-09-02 11:12:25 -0300

    \o/

--- BD/03_SELECAO_BD.sql
+++ BD/03_SELECAO_BD.sql
@@ -8,6 +8,6 @@ SELECT J.NomeJogo AS Jogo,FORMAT( J.DataLancamento, 'dd/MM/yyyy') AS DataLancame
 																																			 JOIN Paises P ON P.PaisId = E.PaisId;
 SELECT J.NomeJogo AS Jogo,FORMAT( J.DataLancamento, 'dd/MM/yyyy') AS DataLancamento,E.NomeEstudio AS Estudio,P.NomePais AS Pais FROM Jogos J FULL JOIN Estudios E ON J.EstudioId = E.EstudioId
 																																			 JOIN Paises P ON P.PaisId = E.PaisId;
-SELECT * FROM Usuarios WHERE Email LIKE '%a%' AND Senha LIKE '%a%';
+SELECT * FROM Usuarios WHERE Email = 'a' AND Senha = 'a';
 SELECT * FROM Jogos WHERE JogoId = 1;
 SELECT * FROM Estudios WHERE EstudioId = 1;
